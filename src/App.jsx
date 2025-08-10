b6584f986cd254ed2e6f2ec3b92f00633f26a042
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2025-08-11 01:27:45 +0330

    setup and configure project routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import ProductsPage from './pages/ProductsPage'
+import DetailsProduct from './pages/DetailsProduct'
+import CheckoutPage from './pages/CheckoutPage'
+import PageNotFound from './pages/404'
 
 function App() {
   return (
-    <div>
-      <h1>MohammadShop</h1>
-    </div>
+    <Routes>
+      <Route path='/' element={<Navigate to='/products' replace/>}/>
+      <Route path='/products' element={<ProductsPage/>}/>
+      <Route path='/products/:id' element={<DetailsProduct/>}/>
+      <Route path='/checkout' element={<CheckoutPage/>}/>
+      <Route path='/*' element={<PageNotFound/>}/>
+    </Routes>
   )
 }
 
---
+++ src/pages/DetailsProduct.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DetailsProduct() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DetailsProduct
